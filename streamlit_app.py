46fff21318bd7aaaf57e6e99b0dbe71efe946505
Aymen Slimani <100181868+eymenslimani@users.noreply.github.com>
2024-12-11 15:06:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ import pandas as pd
 
 st.title('Grocery Recommendation')
 st.info('an app for grocery product recommendation')
-df = pd.read_csv('https://raw.githubusercontent.com/eymenslimani/data/refs/heads/main/Groceries_dataset.csv')
-df
+with st.expander('Data') : 
+  st.write('raw data')
+  df = pd.read_csv('https://raw.githubusercontent.com/eymenslimani/data/refs/heads/main/Groceries_dataset.csv')
+  df
