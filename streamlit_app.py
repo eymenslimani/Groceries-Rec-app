0c5221fb41a58e1c07e4437c902c6c02a8d8373d
Aymen Slimani <100181868+eymenslimani@users.noreply.github.com>
2024-12-11 14:21:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title('Grocery Recommendation')
 st.info('an app for grocery product recommendation')
-
+df = pd.read_csv('https://raw.githubusercontent.com/eymenslimani/data/refs/heads/main/Groceries_dataset.csv')
+df
